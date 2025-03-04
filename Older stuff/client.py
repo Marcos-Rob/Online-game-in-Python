431cda0986cafd8b525d8b41abdf9e6a71767611
Marcos-Rob <marcosrob.pro@gmail.com>
2025-03-04 19:34:00 -0300

    Versão funcionando lan e cores diferentes dos quadrados. Pastas mais organizadas

--- Older stuff/client.py
+++ Older stuff/client.py
@@ -39,7 +39,6 @@ class Player():
     def update(self):    
         self.rect = (self.x, self.y, self.width, self.height)
 
-
 def read_pos(str):
     str = str.split(",")
     return int(str[0]), int(str[1])
